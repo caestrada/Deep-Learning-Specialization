de318e2ec13cbf34bf28034025f8b79eb37e9091
Carlos Estrada <cestr008@gmail.com>
2023-12-08 10:18:34 -0500

    update util

--- 00_neural-networks-and-deep-learning/practice/utils.py
+++ 00_neural-networks-and-deep-learning/practice/utils.py
@@ -1,65 +1,58 @@
-import os
-import numpy as np
-from skimage.io import imread
-from skimage.transform import resize
-from fastai.vision.all import *
-
-def load_dataset(folder_path='bird_or_not', image_size=(64, 64)):
-    categories = ['bird', 'forest']
-    images = []
-    labels = []
-
-    # Iterate over categories
-    for label, category in enumerate(categories):
-        category_path = os.path.join(folder_path, category)
-        for file in os.listdir(category_path):
-            # Load and resize the image
-            img_path = os.path.join(category_path, file)
-            image = imread(img_path)
-            image = resize(image, image_size, anti_aliasing=True)
-            
-            # Append to dataset
-            images.append(image)
-            labels.append(label)
-
-    # Convert to numpy arrays
-    images = np.array(images)
-    labels = np.array(labels)
-
-    # Split into training and testing sets
-    split_idx = int(0.8 * len(images))  # 80% for training, 20% for testing
-    train_set_x_orig = images[:split_idx]
-    train_set_y_orig = labels[:split_idx]
-    test_set_x_orig = images[split_idx:]
-    test_set_y_orig = labels[split_idx:]
-
-    # Reshape labels
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-
-    # Classes array
-    classes = np.array(categories)
-
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
-
-
-import os
 import numpy as np
 from PIL import Image
+import os
 
-def load_and_resize_images():
-    directory = 'cat_or_not'
-    images = []
-    for filename in os.listdir(directory):
-        if filename.endswith('.jpg'):
-            img = Image.open(os.path.join(directory, filename))
-            img = img.resize((64, 64))
-            img_array = np.array(img)
-            images.append(img_array)
-    return images
+def load_dataset2(directory, seed=None, image_size=(64, 64)):
+    train_set_x_orig = []
+    train_set_y = []
+    test_set_x_orig = []
+    test_set_y = []
+    classes = ['forest', 'bird']
 
-bird_images = load_and_resize_images('cat_or_not/bird')
-forest_images = load_and_resize_images('cat_or_not/forest')
+    # Set the seed for reproducibility
+    if seed is not None:
+        np.random.seed(seed)
 
-all_images = np.array(bird_images + forest_images)
-print(all_images.shape)
+    for index, class_name in enumerate(classes):
+        class_dir = os.path.join(directory, class_name)
+        all_images = os.listdir(class_dir)
+
+        # Split data into training and testing (e.g., 80% training, 20% testing)
+        split_index = int(0.8 * len(all_images))
+        train_images = all_images[:split_index]
+        test_images = all_images[split_index:]
+
+        for image_name in train_images:
+            image_path = os.path.join(class_dir, image_name)
+            image = Image.open(image_path)
+            image = image.resize(image_size)  # Resize to 64x64 or any desired size
+            train_set_x_orig.append(np.array(image))
+            train_set_y.append(index)
+
+        for image_name in test_images:
+            image_path = os.path.join(class_dir, image_name)
+            image = Image.open(image_path)
+            image = image.resize(image_size)
+            test_set_x_orig.append(np.array(image))
+            test_set_y.append(index)
+
+    # Combine images and labels into tuples for training and testing sets
+    combined_train = list(zip(train_set_x_orig, train_set_y))
+    combined_test = list(zip(test_set_x_orig, test_set_y))
+
+    # Shuffle the combined lists
+    np.random.shuffle(combined_train)
+    np.random.shuffle(combined_test)
+
+    # Separate the images and labels after shuffling
+    train_set_x_orig, train_set_y = zip(*combined_train)
+    test_set_x_orig, test_set_y = zip(*combined_test)
+
+    # Convert them back to numpy arrays
+    train_set_x_orig = np.array(train_set_x_orig)
+    train_set_y = np.array(train_set_y).reshape((1, -1))
+    test_set_x_orig = np.array(test_set_x_orig)
+    test_set_y = np.array(test_set_y).reshape((1, -1))
+
+    return train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, np.array(classes)
+
